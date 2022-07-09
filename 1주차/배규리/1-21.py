786705c47cca9fbe9553514e413eb11affdac762
배규리 <32202018@dankook.ac.kr>
2022-07-09 18:25:50 +0900

    Update 1-21.py

--- 1주차/배규리/1-21.py
+++ 1주차/배규리/1-21.py
@@ -1,14 +1,7 @@
-#a부터 b까지 정수의 합 구하기 1
-print('a부터 b까지의 정수의 합을 구합니다.')
-a=int(input('정수 a를 입력하세요:'))
-b=int(input('정수 b를 입력하세요:'))
-if a>b:
-    a,b=b,a
-sum=0
-for i in range(a,b+1):
-    if i<b:
-        print(f'{i} + ',end=' ')
-    else:
-        print(f'{i} = ',end=' ')
-    sum+=i
-print(sum)
+#구구단 곱셈표 출력하기
+print('-'*27)
+for i in range(1,10):
+    for j in range(1,10):
+        print(f'{i*j:3}',end=' ')
+    print()
+print('-'*27)
