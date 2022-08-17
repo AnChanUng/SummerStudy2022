ce257805b92ee3bb0e13041a51688e7525095527
Eunyu <82995817+ChunEunyu@users.noreply.github.com>
2022-08-17 14:41:56 +0900

    Update 11721.py

--- 7주차/천은유/11721.py
+++ 7주차/천은유/11721.py
@@ -4,7 +4,6 @@ input = sys.stdin.readline
 s = input().rstrip()
 
 for i in range(0, len(s)):
+    print(s[i], end='')
     if i % 10 == 9:
-        print(s[i], end='\n')
-    else:
-        print(s[i], end='')
+        print()
